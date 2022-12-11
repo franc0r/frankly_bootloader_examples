0c81a2afa58e21468faaf5d3700c44803cb30f37
Martin Bauernschmitt <martin.bauernschmitt@posteo.de>
2022-12-11 12:38:00 +0100

    Moved .vscode config to subfolder + added cortex-m debug support

--- stm_nucleo_g431rb/Core/Src/bootloader_api.cpp
+++ stm_nucleo_g431rb/Core/Src/bootloader_api.cpp
@@ -14,17 +14,92 @@
 
 #include <francor/franklyboot/handler.h>
 
+#include <array>
+
 #include "device_defines.h"
 #include "stm32g4xx.h"
 
 using namespace franklyboot;
 
+// Defines ------------------------------------------------------------------------------------------------------------
+
+constexpr uint32_t MSG_TIMEOUT_CNT = {16000U / 2U};
+constexpr uint32_t MSG_SIZE = {8U};
+
 // Private Variables --------------------------------------------------------------------------------------------------
 
 // Private Function Prototypes ----------------------------------------------------------------------------------------
 
-static void waitForMessage(msg::Msg& request) { (void)request; }
-static void transmitResponse(const msg::Msg& response) { (void)response; }
+/**
+ * @brief Block until message is received from serial line
+ */
+static void waitForMessage(msg::Msg& request) {
+  std::array<std::uint8_t, MSG_SIZE> buffer;
+  uint32_t buffer_idx = 0U;
+  uint32_t timeout_cnt = 0U;
+
+  for (;;) {
+    const uint8_t rx_new_byte = ((LPUART1->ISR & USART_ISR_RXNE) == USART_ISR_RXNE);
+    if (rx_new_byte) {
+      buffer[buffer_idx] = LPUART1->RDR;
+      buffer_idx++;
+
+      if (buffer_idx >= buffer.size()) {
+        return;
+      }
+
+      timeout_cnt = 0U;
+    } else {
+      // Check if new message has started
+      // If new byte is not received within a timeout cnt
+      // the message is ignored
+      if (buffer_idx != 0) {
+        timeout_cnt++;
+
+        if (timeout_cnt >= MSG_TIMEOUT_CNT) {
+          buffer_idx = 0U;
+        }
+      }
+    }
+  }
+
+  /* Decode message */
+  const uint16_t rx_request_raw = (static_cast<uint16_t>(buffer[0U]) << 8U) | static_cast<uint16_t>(buffer[1U]);
+  request.request = static_cast<msg::RequestType>(rx_request_raw);
+  request.packet_id = static_cast<uint8_t>(buffer[2U]);
+  request.data[0U] = static_cast<uint8_t>(buffer[4U]);
+  request.data[1U] = static_cast<uint8_t>(buffer[5U]);
+  request.data[2U] = static_cast<uint8_t>(buffer[6U]);
+  request.data[3U] = static_cast<uint8_t>(buffer[7U]);
+}
+
+/**
+ * @brief Transmit response over serial line
+ */
+static void transmitResponse(const msg::Msg& response) {
+  std::array<std::uint8_t, MSG_SIZE> buffer;
+
+  /* Encode message */
+  buffer[0U] = static_cast<uint8_t>(response.request >> 8U);
+  buffer[1U] = static_cast<uint8_t>(response.request);
+  buffer[2U] = response.packet_id;
+  buffer[3U] = static_cast<uint8_t>(response.response);
+
+  buffer[4U] = response.data.at(0);
+  buffer[5U] = response.data.at(1);
+  buffer[6U] = response.data.at(2);
+  buffer[7U] = response.data.at(3);
+
+  /* Transmit message */
+  for (uint32_t buffer_idx = 0U; buffer_idx < buffer.size(); buffer_idx++) {
+    uint8_t tx_not_ready = ((LPUART1->ISR & USART_ISR_TXE) == USART_ISR_TXE);
+    do {
+      tx_not_ready = !((LPUART1->ISR & USART_ISR_TXE) == USART_ISR_TXE);
+    } while (tx_not_ready);
+
+    LPUART1->TDR = buffer.at(buffer_idx);
+  }
+}
 
 // Public Functions ---------------------------------------------------------------------------------------------------
 
